dcdd79cc2f17f6bf3ef332bc1672cf122fbaaa8d
WiiLife <william.swiss.sy@gmail.com>
2026-04-14 08:52:42 +0200

    made a minimal readme and refactored in main

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+### DEV
+
+requirements
+- python
+- node
+
+getting started
+```
+python -m venv .venv    # create a python virtual environment
+pip install -r requirements.txt
+
+python -m api.main  # start the api
+cd frontend & npm run dev
+```
+
+### DuckDB GUI
+
+a visual helper to access a duckdb
+```
 duckDB gui: docker run -p 5522:5522 ghcr.io/caioricciuti/duck-ui
+```
--- api/main.py
+++ api/main.py
@@ -1,26 +1,8 @@
-from pathlib import Path
 from fastapi import FastAPI
 from api.db import repository
-from api.db.client import dbClient
-import asyncio
 
 
 app = FastAPI()
-db_file_path = Path("./cubi_UDSC_01_csv/cubi_UDSC_01.csv")
-client = dbClient(db_file_path)
-
-async def insert_data():
-    await client.initialize()
-    async with client.aquire() as conn:
-        conn.sql(
-        f"""
-        CREATE TABLE my_table AS
-        SELECT *
-        FROM read_csv_auto({str(db_file_path)});
-        """
-        )
-
-asyncio.run(insert_data())
 
 @app.get("/")
 async def home():
@@ -28,5 +10,9 @@ async def home():
     return {"tables": tables}
 
 if __name__ == "__main__":
+    import api.initiliize
     import uvicorn    
+    import asyncio
+
+    asyncio.run(api.initiliize.insert_data())
     uvicorn.run("api.main:app", port=8000, reload=True)
